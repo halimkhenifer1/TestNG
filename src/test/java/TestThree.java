25fc450e6897bc0056d80eefc53c55f202d00385
halimkhenifer1 <halimkhenifer@outlook.com>
2023-05-28 12:38:32 -0400

    commit

---
+++ src/test/java/TestThree.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class TestThree {
+    @Test
+    public void test5(){
+        System.out.println("Test5");
+    }
+}
--- src/test/java/testClassTwo.java
+++ src/test/java/testClassTwo.java
@@ -1,6 +1,6 @@
 import org.testng.annotations.Test;
 
-public class testClassTwo {
+public class testClassTwo extends BaseClassTest{
     @Test
     public void test4(){
         System.out.println("inside test 4");
